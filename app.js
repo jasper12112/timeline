e2872f41237fd617df5a2ef09ac7af90d42b4a4e
jasper12112 <jasperdijk27@gmail.com>
2023-08-28 20:47:07 +0200

    JJM

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ let title = document.getElementById('title');
 let image_holder = document.getElementById('image-holder');
 // background: linear-gradient(.25turn, #FF9B9B 0 20%, #D9D9D9 20% 100%);
 
-const f_q = "When I'm with you, all my pain would just <span class='gaze'>erase</span>.<br>Days left me thinking about you, leaving my mind like a <span class='gaze'>maze</span>.<br>Remebered you starring at me, having that lovely <span class='gaze'>gaze</span>.<br>By looking at you I found myself at full <span class='gaze'>amaze</span>.<br>Thats when I knew, I knew my heart had found its <span class='gaze'>place</span>"
-const f_e = "Love is blinding...<br>But when Im with you,<br>I see that nothing else matters"
-const f_r = "Your <span class='smile'>smile</span> that brightens up my day.<br>Your <span class='laugh'>laugh</span> that made me forget and <span class='smile'>smile</span>.<br>Your happines that made me <span class='laugh'>laugh</span>"
+const f_q = "When I'm with you, all my pain would just <span class='gaze'>erase</span>.<br>Days left me thinking about you, leaving my mind like a <span class='gaze'>maze</span>.<br>Remebered you starring at me, having that lovely <span class='gaze'>gaze</span>.<br>The moment I knew I would walk for you through all the <span class='gaze'>flames</span>.<br>Thats when I knew, I knew my heart had found its <span class='gaze'>place</span>"
+const f_e = "They say that love is <span class='seeing'>blinding</span>...<br>But the moment I met you,<br>I <span class='seeing'>saw</span> that nothing else matters"
+const f_r = "Your <span class='smile'>smile</span> that brightens up my day.<br>Your <span class='laugh'>laugh</span> that made me forget and <span class='smile'>smile</span>.<br>Your <span class='happines'>happines</span> that made me <span class='laugh'>laugh</span><br>Our time together, that gave me <span class='happines'>happines</span>"
 const f_w = "Before I knew you, I thought I knew love.<br>Your eyes, bright as day.<br>Smile as mesmerizing as the ocean.<br>That's when I knew..."
 const f_t = "Your face never left my <span class='mind'>mind</span>.<br>Conversations that kept flowing in my <span class='mind'>mind</span>.<br>For you, you just never left my <span class='mind'>mind...</span>"
+const f_y = ""
 
 const quotes = [f_q, f_e, f_r, f_w, f_t];
 
