df966f65875f1ffbbbcc63c4bdff99455ed981d5
jasper12112 <jasperdijk27@gmail.com>
2023-09-03 14:41:00 +0200

    changed some phrashes here and there

--- app.js
+++ app.js
@@ -3,17 +3,28 @@ let title = document.getElementById('title');
 let image_holder = document.getElementById('image-holder');
 // background: linear-gradient(.25turn, #FF9B9B 0 20%, #D9D9D9 20% 100%);
 
+const t_q = "Her gaze"
+const t_e = "Blinding love"
+const t_r = "Its you"
+const t_w = "What is love"
+const t_t = "Eternal way"
+const t_y = "She is everything"
+
 const f_q = "When I'm with you, all my pain would just <span class='gaze'>erase</span>.<br>Days left me thinking about you, leaving my mind like a <span class='gaze'>maze</span>.<br>Remebered you starring at me, having that lovely <span class='gaze'>gaze</span>.<br>The moment I knew I would walk for you through all the <span class='gaze'>flames</span>.<br>Thats when I knew, I knew my heart had found its <span class='gaze'>place</span>"
 const f_e = "They say that love is <span class='seeing'>blinding</span>...<br>But the moment I met you,<br>I <span class='seeing'>saw</span> that nothing else matters"
-const f_r = "Your <span class='smile'>smile</span> that brightens up my day.<br>Your <span class='laugh'>laugh</span> that made me forget and <span class='smile'>smile</span>.<br>Your <span class='happines'>happines</span> that made me <span class='laugh'>laugh</span><br>Our time together, that gave me <span class='happines'>happines</span>"
+const f_r = "Your <span class='smile'>smile</span> that brightens my day.<br>Your <span class='laugh'>laugh</span> that made me forget and <span class='smile'>smile</span>.<br>Your <span class='happines'>happines</span> that made me <span class='laugh'>laugh</span><br>Our time together, that gave me <span class='happines'>happines</span>"
 const f_w = "Before I knew you, I thought I knew love.<br>Your eyes, bright as day.<br>Smile as mesmerizing as the ocean.<br>That's when I knew..."
-const f_t = "Your face never left my <span class='mind'>mind</span>.<br>Conversations that kept flowing in my <span class='mind'>mind</span>.<br>For you, you just never left my <span class='mind'>mind...</span>"
-const f_y = ""
+const f_t = "For when all seemed dark, her eyes shined bright<br>When it seemed like there was no way, you were my light<br>For even death can't stop us, I know we will reunite"
+const f_y = "You are the star that guides me through night,<br>You are the light that brightens my day,<br>You are the path leads my way"
 
-const quotes = [f_q, f_e, f_r, f_w, f_t];
+const f_t_old = "Your face never left my <span class='mind'>mind</span>.<br>Conversations that kept flowing in my <span class='mind'>mind</span>.<br>For you, you just never left my <span class='mind'>mind...</span>"
+
+const quotes_title = [t_q, t_y, t_r, t_w, t_t, t_e];
+const quotes = [f_q, f_y, f_r, f_w, f_t, f_e];
 
 let current_quote = 0;
 const quote_element = document.getElementById("quote");
+const quote_title_element = document.getElementById("quote-title");
 
 function buttonclick(index)
 {
@@ -50,7 +61,7 @@ function buttonclick(index)
             innerslider.style.width = "100.1%";
             break;
     }
-    setNewQuote();
+    setNewQuote(index - 1);
 }
 
 function hidden(index)
@@ -88,11 +99,13 @@ const initialize_quote = () => {
     //     setNewQuote();
     // }
     quote_element.innerHTML = quotes[current_quote];
+    quote_title_element.innerHTML = "** " + quotes_title[current_quote] + " **";
 }
 
-const setNewQuote = () => {
-    current_quote = current_quote == quotes.length - 1 ? 0 : current_quote + 1;
-    quote_element.innerHTML = quotes[current_quote];
+const setNewQuote = (index) => {
+    current_quote = index > quotes.length ? quotes.length : index;
+    quote_element.innerHTML = quotes[index];
+    quote_title_element.innerHTML = "** " + quotes_title[index] + " **";
 }
 
 initialize_quote();
